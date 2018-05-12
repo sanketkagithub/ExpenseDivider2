8bc1784ef50c3683f316ceea67e0cfa5fbea3a60
sanketkagithub <sanketishere101@gmail.com>
2018-05-13 00:35:31 +0530

    sdsd

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,7 +1,7 @@
 # Add project specific ProGuard rules here.
 # You can control the set of applied configuration files using the
 # proguardFiles setting in build.gradle.
-#-dontwarn info.guardianproject.netcipher.**
+-dontwarn info.guardianproject.netcipher.**
 # For more details, see
 #   http://developer.android.com/guide/developing/tools/proguard.html
 #-dontwarn sun.misc.Unsafe
--- app/src/main/java/com/busyprojects/roomies/roomyActivities/RegisterLoginActivity.java
+++ app/src/main/java/com/busyprojects/roomies/roomyActivities/RegisterLoginActivity.java
@@ -34,44 +34,22 @@ import java.util.List;
 
 public class RegisterLoginActivity extends Activity {
     Context context = RegisterLoginActivity.this;
-
-    EditText et_register, et_login;
-    //   TextView tv_log_reg_title;
-    AnimationManager animationManager;
+    //AnimationManager animationManager;
 
 
-    DatabaseReference db_ref;
 
-    DialogEffect dialogEffect;
-
-    SharedPreferences sp;
-    SharedPreferences.Editor spe;
+   // SharedPreferences.Editor spe;
 
     RuntimePermissionsCs runtimePermissionsCs;
-    // boolean CheckInternetReceiver.isOnline(this);
-   // RelativeLayout rel_login_reg, rel_login, rel_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_login);
 
-        sp = getSharedPreferences(SessionManager.FILE_WTC, MODE_PRIVATE);
-//        CheckInternetReceiver.isOnline(this) = sp.getBoolean(SessionManager.IS_INTERNET, false);
-        //CheckInternetReceiver.isOnline(this) = CheckInternetReceiver.isOnline(this);
-        db_ref = Helper.getFirebaseDatabseRef();
 
-        dialogEffect = new DialogEffect(context);
+        //animationManager = AnimationManager.getInstance();
 
-        et_login = findViewById(R.id.et_login);
-        et_register = findViewById(R.id.et_register);
-
-        //tv_log_reg_title = findViewById(R.id.tv_log_reg_title);
-        animationManager = AnimationManager.getInstance();
-
-//        rel_login_reg = findViewById(R.id.rel_log_reg);
-//        rel_login = findViewById(R.id.rel_login);
-//        rel_register = findViewById(R.id.rel_register);
 
 
         if (Build.VERSION.SDK_INT >= 23) {
@@ -81,196 +59,8 @@ public class RegisterLoginActivity extends Activity {
     }
 
 
-    String mobileLogin;
 
 
- /*   public void loginRoomyold(View view) {
-
-        if (CheckInternetReceiver.isOnline(this)) {
-            mobileLogin = et_login.getText().toString();
-
-            if (mobileLogin.equals("")) {
-                Toast.makeText(context, "Please Check Empty Fields", Toast.LENGTH_SHORT).show();
-
-            } else {
-
-                dialogEffect.showDialog();
-                final List<String> userListMobile = new ArrayList<>();
-                // TODO: 1/28/2018 validate login mobile
-                db_ref.child(Helper.USER)
-                        .addValueEventListener(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                                dialogEffect.cancelDialog();
-                                userListMobile.clear();
-
-                                for (DataSnapshot dataSnapshot1 :
-                                        dataSnapshot.getChildren()) {
-                                    User user = dataSnapshot1.getValue(User.class);
-                                    userListMobile.add(user.getMobile());
-                                }
-
-                                if (userListMobile.contains(mobileLogin)) {
-
-                                    // TODO: 1/28/2018 save main roomate (user) in session
-                                    spe = sp.edit();
-                                    spe.putString(SessionManager.MOBILE, mobileLogin);
-                                    spe.apply();
-
-                                    SharedPreferences spUc = getSharedPreferences(SessionManager.FILE_UC, MODE_PRIVATE);
-
-                                    spe = spUc.edit();
-                                    spe.putString(SessionManager.LOGGED_MOBILE_UC, mobileLogin);
-                                    spe.apply();
-
-
-                                    saveRoomyMacAddressForNotification();
-
-
-                                    startActivity(new Intent(context, HomeActivity.class));
-
-
-                                } else {
-
-                                    // TODO: 3/24/2018  open admin
-                                    if (mobileLogin.equals("0000")) {
-
-
-                                    } else {
-                                        Toast.makeText(context, "Invalid Login", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-
-                            }
-
-                            @Override
-                            public void onCancelled(DatabaseError databaseError) {
-
-                                Toast.makeText(context, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-
-            }
-
-        } else {
-            Helper.showCheckInternet(context);
-
-        }
-    }
-*/
-
-    void saveRoomyMacAddressForNotification() {
-        String macAddress = Helper.getMacAddr();
-
-        if (macAddress.equals("")) {
-            macAddress = "emulatorMacAdd";
-        }
-        spe = sp.edit();
-        spe.putString(SessionManager.MAC_ADDRESS, macAddress);
-        spe.apply();
-
-
-        SharedPreferences spUc = getSharedPreferences(SessionManager.FILE_UC, MODE_PRIVATE);
-        int payNotfSize = spUc.getInt(SessionManager.PNID_LIST, 0);
-
-        System.out.println(payNotfSize + " payNotfSize ");
-
-        String pnId = Helper.randomString(10);
-        PaymentNotification paymentNotification = new PaymentNotification();
-        paymentNotification.setMacAddress(macAddress);
-        paymentNotification.setMobileLogged(mobileLogin);
-        paymentNotification.setPnid(pnId);
-
-        HashMap<String, Payment> paymentMap = new HashMap<>();
-
-        // TODO: 3/1/2018  dummy Payment
-        Payment payment = new Payment();
-        payment.setPid("xdvxcvcxcv");
-        payment.setMobileLogged(mobileLogin);
-
-        for (int i = 0; i < payNotfSize; i++) {
-            paymentMap.put(i + "defaultKeys", payment);
-        }
-
-
-        paymentNotification.setPaymentList(paymentMap);
-
-        db_ref.child(Helper.PAYMENT_NOTIFICATION)
-                .child(macAddress)
-                .setValue(paymentNotification);
-
-
-        //getRoomsAllMacAddressListInSession();
-
-
-    }
-
-/*
-    public void registerRoomy(View view) {
-
-if (CheckInternetReceiver.isOnline(this)) {
-    final String mobileReg = et_register.getText().toString();
-
-    if (mobileReg.equals("")) {
-        Toast.makeText(context, "Please Check Empty Fields", Toast.LENGTH_SHORT).show();
-    } else {
-
-
-        db_ref.child(Helper.USER)
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-
-                        List<String> userList = new ArrayList<>();
-
-                        for (DataSnapshot dataSnapshot1 :
-                                dataSnapshot.getChildren()) {
-
-                            User userDb = dataSnapshot1.getValue(User.class);
-
-                            userList.add(userDb.getMobile());
-
-                        }
-
-
-                        if (userList.contains(mobileReg)) {
-                            Toast.makeText(context, "User Already Exists", Toast.LENGTH_SHORT).show();
-                        } else {
-                            // TODO: 2/25/2018 save user
-                            String uid = Helper.randomString(10);
-                            User user = new User();
-                            user.setUid(uid);
-                            user.setMobile(mobileReg);
-
-                            db_ref.child(Helper.USER).child(uid)
-                                    .setValue(user);
-                            Toast.makeText(context, "User Registered Successfully", Toast.LENGTH_SHORT).show();
-
-                            sp.edit().putString(SessionManager.MOBILE, mobileReg).apply();
-
-
-                        }
-
-
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-
-
-    }
-
-}else
-{
-    Helper.showCheckInternet(context);
-
-}
-    }
-*/
 
 
     String[] PERMISSIONS = {android.Manifest.permission.CALL_PHONE,
